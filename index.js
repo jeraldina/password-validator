406809da2a63aeb9fbc49b4e625cc02d822c6dde
jeraldina <jeraldina@gmail.com>
2021-07-17 23:02:09 -0400

    adding if password is less than 8 and lower and uppercase. Currently trying out variables with letters as strings to see if that will help run my function.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-function validatePassword(){
-    
+function validatePassword(password){
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+const specialChar = '!@#$%^&*()_+-={[}]|\:;<./>?,`~'
+
+if (password.length < 8) return false
+    else return true
+
+if (password(lowerCase.includes.lowerCase)) return true
+    else return false
+
+if (password.includes(upperCase)) return true
+    else return false
+
+if (password.includes(specialChar)) return true
+    else return false
+
+// if password.//( has at least one lowercase)
+// if password.//( has at least one uppercase)
+// if password.//( has at least one numeric value)
+// if password.//( has at least one special character)
 }
+
+module.exports = validatePassword
