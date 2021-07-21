101e93052d5b2cec1aba0fbc4f86310438a15392
jeraldina <jeraldina@gmail.com>
2021-07-21 19:02:21 -0400

    adding functions to check for pw length, upper case, lower case and special char

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
+const { test } = require("mocha")
+
 function validatePassword(password){
-const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const lowerCase = 'abcdefghijklmnopqrstuvwxyz'
-const specialChar = '!@#$%^&*()_+-={[}]|\:;<./>?,`~'
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const specialChar = '!@#$%^&*()<>?_+}{-=[]\|,./`~'
 
-if (password.length < 8) return false
-    else return true
+let checkLength = false
+let checkLowerCase = true
+let checkSpecialChar = true
+let checkUpperCase = true
 
-if (password(lowerCase.includes.lowerCase)) return true
-    else return false
-
-if (password.includes(upperCase)) return true
-    else return false
-
-if (password.includes(specialChar)) return true
-    else return false
-
-// if password.//( has at least one lowercase)
-// if password.//( has at least one uppercase)
-// if password.//( has at least one numeric value)
-// if password.//( has at least one special character)
+function passwordLength(){
+if (password.length < 8) return checkLength
 }
 
+function passwordLowerCase(){
+if (password.includes(lowerCase)){
+
+    return checkLowerCase
+} 
+}
+// console.log = 
+
+function passwordUpperCase(){
+if (password.includes(upperCase)){
+
+    return checkUpperCase
+} 
+}
+function passwordSpecialChar(){
+if (password.includes(specialChar)){
+
+    return checkSpecialChar
+} 
+}
+
+//hold variable with answer true
+// 
+return checkLength && checkLowerCase && checkUpperCase && checkSpecialChar
+}
 module.exports = validatePassword
+
+// arrayDiff(testArrayOne, testArrayTwo)
+// let thirdArrayAnswer = []
+
+// for (let i = 0; i < arrayDiff.length; i++)
+// if (testArrayOne[i] not in testArrayTwo)
+// thirdArrayAnswer.push(testArrayOne[i])
